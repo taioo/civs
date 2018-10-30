4edbadd242b7e1d7a56799b6bab77ca9912b278d
taioo <thaer.tai@gmail.com>
2018-10-30 19:27:46 +0100

    fin u5

--- u5/a1.py
+++ u5/a1.py
@@ -1,17 +1,26 @@
 import cv2
 import numpy as np	
+
+#Aufloesung 640x480
+img = np.zeros([480,640])
+img[:] = 255
+
+
+# 3D points
+x1 = np.array(([10.],[10.],[100.]))
+x2 = np.array(([33.],[22.],[111.]))
+x3 = np.array(([100.],[100.],[1000.]))
+x4 = np.array(([20.],[-100.],[100.]))
+points3D = np.array([x1,x2,x3,x4])
+x_img = cv2.imread("x.png")
+
+
+# Kalibrierung
 f = 460
 cx = 320
 cy = 240
-#Aufloesung 640x480
-#X1=(10,10,100), X2=(33,22,111), X3=(100,100,1000), X4=(20,-100,100)
 
-x1_3D = np.array(([10.],[10.],[100.]))
-x2_3D = np.array(([33.],[22.],[111.]))
-x3_3D = np.array(([100.],[100.],[1000.]))
-x4_3D = np.array(([20.],[-100.],[100.]))
-
-#Define K
+# K
 K = np.zeros((3,3))
 K[0,0] = f
 K[1,1] = f
@@ -19,40 +28,58 @@ K[0,2] = cx
 K[1,2] = cy
 K[2,2] = 1
 
-#Define P
+
+#Rotationsmatrix
 R = np.eye(3)
-t = np.array([[ 0.],[ 0.],[ 0.]])
+
+#Translationsvektor
+t = np.zeros((3,1))
+
+#[R|t]
 Rt = np.hstack((R,t))
-P = K.dot(Rt)
 
-# 3D kartesisch -> 3D homogen
-x1_3D_homo = np.vstack ((x1_3D, np.array([[1.]])))
-x2_3D_homo = np.vstack ((x2_3D, np.array([[1.]])))
-x3_3D_homo = np.vstack ((x3_3D, np.array([[1.]])))
-x4_3D_homo = np.vstack ((x4_3D, np.array([[1.]])))
+# P = K[R|t]
+P = np.dot(K,Rt)
 
-# 3D homogen -> 2D homogen
-x1_2D_homo = P.dot(x1_3D_homo)
-x2_2D_homo = P.dot(x2_3D_homo)
-x3_2D_homo = P.dot(x3_3D_homo)
-x4_2D_homo = P.dot(x4_3D_homo)
+# katesisch -> homogen
+x1 = np.vstack ((x1, np.array([[1.]])))
+x2 = np.vstack ((x2, np.array([[1.]])))
+x3 = np.vstack ((x3, np.array([[1.]])))
+x4 = np.vstack ((x4, np.array([[1.]])))
 
-# 2D homogen -> 2D katesisch
-x1_2D = np.array([np.rint(x1_2D_homo[0]/x1_2D_homo[2]),np.rint(x1_2D_homo[1]/x1_2D_homo[2])])
-x2_2D = np.array([np.rint(x2_2D_homo[0]/x2_2D_homo[2]),np.rint(x2_2D_homo[1]/x2_2D_homo[2])])
-x3_2D = np.array([np.rint(x3_2D_homo[0]/x3_2D_homo[2]),np.rint(x3_2D_homo[1]/x3_2D_homo[2])])
-x4_2D = np.array([np.rint(x4_2D_homo[0]/x4_2D_homo[2]),np.rint(x4_2D_homo[1]/x4_2D_homo[2])])
+# x = PX       3D -> 2D
+x1 = np.dot(P,x1)
+x2 = np.dot(P,x2)
+x3 = np.dot(P,x3)
+x4 = np.dot(P,x4)
+
+#homogen -> katesisch
+x1 = np.array([np.rint(x1[0]/x1[2]),np.rint(x1[1]/x1[2])])
+x2 = np.array([np.rint(x2[0]/x2[2]),np.rint(x2[1]/x2[2])])
+x3 = np.array([np.rint(x3[0]/x3[2]),np.rint(x3[1]/x3[2])])
+x4 = np.array([np.rint(x4[0]/x4[2]),np.rint(x4[1]/x4[2])])
 
 print "Aufgabe 1:"
-print ("X1 = " + str(x1_2D))
-print ("X2 = " + str(x2_2D))
-print ("X3 = " + str(x3_2D))
-print ("X4 = " + str(x4_2D))
+print ("X1 = " + str(x1))
+print ("X2 = " + str(x2))
+print ("X3 = " + str(x3))
+print ("X4 = " + str(x4))
 
 print "Aufgabe 2:"
-points3D = np.array([x1_3D, x2_3D,x3_3D,x4_3D])
 points2D = cv2.projectPoints(points3D, np.array([0.,0.,0.]), np.array([0.,0.,0.]), K, None)
 print ("X1 = " + str(np.rint(points2D[0][0])))
 print ("X2 = " + str(np.rint(points2D[0][1])))
 print ("X3 = " + str(np.rint(points2D[0][2])))
 print ("X4 = " + str(np.rint(points2D[0][3])))
+
+
+img = points2D[0][0]
+img = points2D[0][2]
+img = points2D[0][3]
+#img = points2D[0][4]
+
+cv2.circle(img,(x1[0],x1[1]), 5, (0,255,0), -1)
+cv2.circle(img,(x2[0],x2[1]), 5, (0,255,0), -1)
+cv2.circle(img,(x3[0],x3[1]), 5, (0,255,0), -1)
+cv2.circle(img,(x4[0],x4[1]), 5, (0,255,0), -1)
+cv2.imwrite('end.png', img)
